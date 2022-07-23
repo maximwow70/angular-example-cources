b0a0f4e294991dea5fa56c436a267a17afef926a
Maksim Samuilionak <maximwow70@gmail.com>
2022-07-23 09:40:17 +0400

    routing iteration 1
    routing iteration 1
    
    
    routing iteration 1

--- Burgers.io/src/app/app-routing.module.ts
+++ Burgers.io/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
+import { BurgerListComponent } from './modules/burger-list/burger-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+  {
+    path: 'burgers',
+    component: BurgerListComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
